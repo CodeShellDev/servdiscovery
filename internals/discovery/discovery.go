55fe6a945a77e09836ed3b2f06d590d7a7e6f393
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-12-05 16:53:43 +0100

    debugging

--- internals/discovery/discovery.go
+++ internals/discovery/discovery.go
@@ -30,9 +30,13 @@ func GetDiffDiscovery() Diff[string] {
 		return Diff[string]{}
 	}
 
+	logger.Dev("Raw: ", diff)
+
 	logger.Debug("Cleaning diff")
 
 	cleaned := CleanDiff(diff)
+
+	logger.Dev("Cleaned: ", cleaned)
 
 	return cleaned
 }
